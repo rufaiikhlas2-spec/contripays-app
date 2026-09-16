15ab93af70983ad230875887e70785ae2100f875
rufaiikhlas2-spec <rufaiikhlas2@gmail.com>
2026-09-16 11:24:54 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.klashworld.contripays',
   appName: 'ContriPays',
-  webDir: '.'
+  webDir: 'www'
 };
 
 export default config;
