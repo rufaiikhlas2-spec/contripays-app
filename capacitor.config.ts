14af736dfdc8c4d308cf38277b98766e1ab74808
rufaiikhlas2-spec <rufaiikhlas2@gmail.com>
2026-09-16 11:06:51 +0100

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.klashworld.contripays',
+  appName: 'ContriPays',
+  webDir: '.'
+};
+
+export default config;
